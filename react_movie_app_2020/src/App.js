af642ac2f7a791cbbeee440e3259e15f3437366d
Solbi Shin <solbi@measureaid.com>
2020-02-12 15:59:12 +0900

    using state

--- react_movie_app_2020/src/App.js
+++ react_movie_app_2020/src/App.js
@@ -1,67 +1,30 @@
 import React from 'react'
-import PropTypes from "prop-types"
 
-function Food({name, picture,rating}) {
-  const imgStyle = {
-    width: '200px',
-    height:'200px'
+//class component
+class App extends React.Component{
+  state = {
+    count: 0 
   }
-  return <div>
-    <h1>I Love {name} </h1>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} style={imgStyle}/>
-  </div>
-  
-}
+  add = () => {
+    // this.setState({count: this.state.count + 1})
+    //current : 현재값을 가져옴
+    this.setState(current => ({count: current.count + 1}))
+  }
+  minus = () => {
+    // this.setState({count: this.state.count - 1})
+    //current : 현재값을 가져옴
+    this.setState(current => ({count: current.count - 1}))
+  }
 
-const foodILike = [
-  {
-    id:1,
-    name:"Chicken",
-    image:"https://www.inspiredtaste.net/wp-content/uploads/2017/10/Roasted-Chicken-with-Lemon-Recipe-1200.jpg",
-    rating:5
-  },
-  {
-    id:2,
-    name:"Kimchi",
-    image:"https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg",
-    rating:4.9
-  },
-  {
-    id:3,
-    name:"Cake",
-    image:"https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/16730-beckys-butter-cake-600x600.jpg?ext=.jpg",
-    rating:3.4
-  },
-  {
-    id:4,
-    name:"Kimbab",
-    image:"http://www.nongsaro.go.kr/ps/img/interabang/num207/headerImg.jpg",
-    rating:4.7
-  },
-]
-
-// function renderFood(dish) {
-//   return <Food name={dish.name} picture={dish.picture}/>
-// }
-
-Food.propTypes = {
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired,
-  //number or undefined
-  rating:PropTypes.number,
-}
-
-
-function App() {
-  return (
+  render() {
+    return (
     <div>
-      <h1> Hello World!</h1> 
-      {foodILike.map(dish => 
-      <Food key={dish.id} name= {dish.name} picture={dish.image} rating={dish.rating}/>
-      )}
-      </div>
-  )
+      <h1>The number is: { this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
+    )
+  }
 }
 
 export default App;
