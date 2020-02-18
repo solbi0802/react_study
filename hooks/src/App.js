d794e84d4b9338b849cc650dc02f62c71f104f51
Solbi Shin <solbi@measureaid.com>
2020-02-18 23:14:40 +0900

    react hook

---
+++ hooks/src/App.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  })
+  
+  return (
+    <div className="App">
+     <p>You clicked {count} times</p>
+     <button onClick={() => setCount(count+1)} >
+       Click me
+     </button>
+    </div>
+  );
+}
+
+export default App;
